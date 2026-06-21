e72862ce4052739aef357585e9bb411f5aa3de5b
Francisco Estevez <franciscoestevez987@gmail.com>
2026-06-21 18:10:58 -0300

    arregla servicio de notas en frontend

--- frontend/src/app/services/notas.service.ts
+++ frontend/src/app/services/notas.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Nota } from '../models/nota.js';
+import { Nota } from '../models/nota';
 
 @Injectable({
   providedIn: 'root'
