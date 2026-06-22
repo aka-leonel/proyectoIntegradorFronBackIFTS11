404e5c86364127c96b6d2dfb0ca95793c8c3413a
54112 <54112@BELENLOPEZBRAVO>
2026-06-22 13:15:24 -0300

    feat: ajustes en rutas y servicio de notas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'notas',
+        pathMatch: 'full'
+    },
+   
+    {
+        path: 'notas/:id',
+        loadComponent: () =>
+            import('./components/notas-detalle/notas-detalle.component')
+            .then(m => m.NotasDetalleComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
