5b52151c242973d75d7976d11cf44f40a0c142f4
Leonel Godoy <leonelgodoy495@gmail.com>
2026-06-23 19:04:10 -0300

    fix: se ajustó el endpoint getNotasController

--- backend/controllers/notasController.js
+++ backend/controllers/notasController.js
@@ -3,13 +3,19 @@ const notasService = require('../services/notasService.js')
 exports.getNotasController = async (req, res) => {
   try {
     const notas = await notasService.getNotasService();
-    if(notas.length === 0) {
+    if (notas.length === 0) {
       return res.status(404).send({
         message: "No se encontraron notas"
       })
     }
     res.status(200).send(notas);
-  } catch (err) { res.status(500).json({ error: 'Error al obtener notas' }); }
+  } catch (err) {
+    res.status(500).send({
+      code: 500,
+      error: 'Error en getNotasController'
+    }
+    )
+  }
 };
 
 exports.getNotaByIdController = async (req, res) => {
@@ -17,50 +23,55 @@ exports.getNotaByIdController = async (req, res) => {
     const id = req.params.id
     const nota = await notasService.getNotaByIdService(id);
     if (!nota) {
-      return res.status(404).json(
+      return res.status(404).send(
         { message: 'No se encontró la nota' }
       )
     };
     res.status(200).send(nota);
-  } catch (err) { res.status(500).json({ error: 'Error' }); }
+  } catch (err) {
+    res.status(500).send({
+      code: 500,
+      error: 'Error en getNotaByIdController'
+    });
+  }
 };
 
 // exports.createNotaController = ...
 
 exports.updateNotaController = async (req, res) => {
-    try {
-        const id = req.params.id
-        const datos = req.body
-        const nota = await notasService.updateNotaService(id, datos)
-        if (!nota) {
-            return res.status(404).send({
-                message: "La nota a actualizar no fue encontrada"
-            })
-        }
-        res.status(200).send(nota)
-    } catch (error) {
-        console.log("Error en updateNotaController: ", error)
-        res.status(500).send({
-            code: 500,
-            message: "Error al actualizar la nota"
-        })
+  try {
+    const id = req.params.id
+    const datos = req.body
+    const nota = await notasService.updateNotaService(id, datos)
+    if (!nota) {
+      return res.status(404).send({
+        message: "La nota a actualizar no fue encontrada"
+      })
     }
+    res.status(200).send(nota)
+  } catch (error) {
+    console.log("Error en updateNotaController: ", error)
+    res.status(500).send({
+      code: 500,
+      message: "Error al actualizar la nota"
+    })
+  }
 }
 exports.deleteNotaController = async (req, res) => {
-    try {
-        const id = req.params.id
-        const nota = await notasService.deleteNotaService(id)
-        if(nota.length === 0) {
-            return res.status(404).send({
-                message: "La nota a eliminar no fue encontrada"
-            })
-        }
-        res.status(200).send(nota)
-    } catch (error) {
-        console.log("Error en deleteNotaController: ", error)
-        res.status(500).send({
-            code: 500,
-            message: "Error al eliminar la nota"
-        })
+  try {
+    const id = req.params.id
+    const nota = await notasService.deleteNotaService(id)
+    if (nota.length === 0) {
+      return res.status(404).send({
+        message: "La nota a eliminar no fue encontrada"
+      })
     }
+    res.status(200).send(nota)
+  } catch (error) {
+    console.log("Error en deleteNotaController: ", error)
+    res.status(500).send({
+      code: 500,
+      message: "Error al eliminar la nota"
+    })
+  }
 }
